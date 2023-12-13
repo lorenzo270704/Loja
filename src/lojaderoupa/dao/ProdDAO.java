43f478c5d5f4c141994d1607048f3121993c220a
ChayRR12 <chayelek@outlook.com>
2023-12-13 20:51:00 -0300

    Criacao de classes

---
+++ src/lojaderoupa/dao/ProdDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lojaderoupa.dao;
+
+/**
+ *
+ * @author 66211020764
+ */
+public class ProdDAO {
+    
+}
