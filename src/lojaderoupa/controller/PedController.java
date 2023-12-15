e1fb85bfd7d54c2ad29c5c2fd46b6156efad9f59
lorenzo270704 <denslobe123@gmail.com>
2023-12-15 20:03:32 -0300

    Controller do pedido

--- src/lojaderoupa/controller/PedController.java
+++ src/lojaderoupa/controller/PedController.java
@@ -4,10 +4,28 @@
  */
 package lojaderoupa.controller;
 
+import javax.swing.JOptionPane;
+import lojaderoupa.model.Cliente;
+import lojaderoupa.model.Pedido;
+import lojaderoupa.model.Produto;
+
 /**
  *
  * @author silvi
  */
 public class PedController {
+    
+    public void Incluir (Cliente cliente, Produto produto,double valorTot, String pagamento){
+        
+          if(!cliente.equals("")&&!produto.equals("")&&!valorTot.equals("")&&!pagamento.equals(""))
+        {
+            Pedido ped = new Pedido(cliente, produto, valorTot, pagamento);
+            ped.Incluir(ped);
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Preencha os campos");       
+    
+    }
+    }  
     
 }
